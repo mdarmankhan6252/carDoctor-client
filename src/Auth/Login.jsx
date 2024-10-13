2ed7d5b6c812c8ae14ab58d4cf597da754470701
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2024-10-13 14:27:17 +0600

    I have created login and register page

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,8 +1,29 @@
+import { VscGithubInverted } from 'react-icons/vsc';
+import img_1 from '../assets/images/login/login.svg'
+import { FcGoogle } from "react-icons/fc";
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
    return (
-      <div>
-         
+      <div className='flex items-center *:basis-1/2 gap-y-10 py-20 sm:flex-row flex-col'>
+         <div>
+            <img src={img_1} alt="" className='w-[90%]' />
+         </div>
+         <form className='border p-10 shadow-[0px_0px_2px_0px] shadow-gray-500 *:w-full'>
+            <h2 className='text-center pb-8 text-2xl font-semibold sm:text-4xl'>Sign In</h2>
+            <label className='font-semibold pb-1'>Email</label>
+            <input type="email" name="email" placeholder='Your Email' className='border p-2 mb-6 ' />
+            <label className='font-semibold pb-1'>Password</label>
+            <input type="password" name="password" placeholder='Your password' className='border p-2 mb-6 ' />
+            <input type="submit" value="Sign In" className='btn_1 cursor-pointer' />
+            <p className='text-center py-3 font-semibold text-gray-600'>Or Sign Up with</p>
+            <div className='flex items-center justify-center text-3xl space-x-5 *:cursor-pointer'>
+               <FcGoogle className='text-4xl'/>
+               <VscGithubInverted />
+            </div>
+            <p className='text-[14px] pt-4 text-center'>Are You New Here ? <Link to='/register' className='font-semibold text-[#FF3811] hover:border-b-2 border-red-400'>Register</Link></p>
+         </form>
       </div>
    );
 };
